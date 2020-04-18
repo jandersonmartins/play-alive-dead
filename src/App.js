50ab4e700ebf0b5da9120341e6e5537a82342718
Janderson Martins <jandersonmartins10@gmail.com>
2020-04-18 18:11:56 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,89 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useReducer, useEffect } from 'react';
+import aliveUrl from './sounds/alive-pt.ogg'
+import deadUrl from './sounds/dead-pt.ogg'
+import aliveImage from './images/alive.svg'
+import deadImage from './images/dead.svg'
 import './App.css';
 
-function App() {
+const audios = [
+  new Audio(aliveUrl),
+  new Audio(deadUrl)
+]
+
+const images = [
+  aliveImage,
+  deadImage
+]
+
+const zeroOrOne = () => Math.round(Math.random())
+
+const INITIAL_STATE = {
+  playing: false,
+  audio: null
+}
+
+const Types = {
+  STOP: 'STOP',
+  PLAY: 'PLAY',
+  CHANGE_AUDIO: 'CHANGE_AUDIO'
+}
+
+const reducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case Types.STOP:
+      return {
+        ...state,
+        playing: false,
+        audio: null
+      }
+
+    case Types.PLAY:
+      return {
+        ...state,
+        playing: true,
+        audio: action.audio
+      }
+
+    case Types.CHANGE_AUDIO:
+      return {
+        ...state,
+        audio: action.audio
+      }
+
+    default:
+      return { ...state }
+  }
+}
+
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, INITIAL_STATE)
+
+  useEffect(() => {
+    if (state.playing) {
+      audios[state.audio].play().then(() => {
+        setTimeout(() => {
+          dispatch({ type: Types.CHANGE_AUDIO, audio: zeroOrOne() })
+        }, 900) // TODO: Get random milliseconds
+      })
+    }
+  }, [state])
+
+  const handleOnClick = () => {
+    if (state.playing) {
+      return dispatch({ type: Types.STOP })
+    }
+    dispatch({ type: Types.PLAY, audio: zeroOrOne() })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="btn-container">
+        <button onClick={handleOnClick}>{state.playing ? 'Parar' : 'Começar'}</button>
+      </div>
+
+      <div className="image-container">
+        {state.playing && <img src={images[state.audio]} alt={state.audio ? 'vivo' : 'morto'} />}
+      </div>
     </div>
   );
 }
